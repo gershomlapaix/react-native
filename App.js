2f1edd4529a4db5821329d4d8ec87b17bfd73e42
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-06-15 21:12:11 +0200

    feat: change

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import { Text, View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import RegisterScreen from './components/Auth/Register'
 import LoginScreen from './components/Auth/Login';
 import RestaurantsList from './components/Pages/RestaurantsList';
 import HomeScreen from './components/Pages/Home';
+import BottomNavScreen from './components/Pages/BottomNav'
 import AssetExample from './components/AssetExample';
 
 const Stack = createNativeStackNavigator();
@@ -14,8 +16,13 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Login" component={LoginScreen}/>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Supa menu app' }}
+        />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Bnav" component={BottomNavScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
