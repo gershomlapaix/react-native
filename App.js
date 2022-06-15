9a60f7b89ed6889cec01a814157ade621be3ad6c
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-06-15 11:26:59 +0200

    feat: app.js

--- App.js
+++ App.js
@@ -1,130 +1,34 @@
-import * as React from 'react'
-import { Text, View, StyleSheet, TextInput, Button, Pressable } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import Login from './components/Auth/Register'
 
-const Login = ()=>{
-  return(
-    <>
-    <View style = {styles.container}>
-      <View style={styles.form}>
-       <View style={styles.formContent}>
-        <View style={styles.welcome}>
-         <View style={styles.appName}>
-          <Text style={styles.appNameText1}>Supa</Text>
-          <Text style={styles.appNameText2}>Menu</Text>
-        </View>
-       <Text>Welcome ...</Text>
-       <Text>Please fill in the information</Text>
+// You can import from local files
+import AssetExample from './components/AssetExample';
 
-        </View>
-       <TextInput placeholder="Full name" style={styles.formInput}/>
-       <TextInput placeholder="Phone number" style={styles.formInput}/>
-       <TextInput placeholder="Your email" style={styles.formInput}/>
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
 
-        <View style={styles.actions}>
-          <Pressable style={styles.btn}>
-            <Text style={styles.btnText}>Proceed</Text>
-            </Pressable>
-
-          <Text>OR</Text>
-          <Text>If you have a PMG account</Text>
-
-         <Pressable style={styles.btn}>
-            <Text>Sign in</Text>
-            </Pressable>
-
-            <View style={styles.actionsRegister}>
-            <Text>Don't have an account? </Text>
-
-            <Pressable style={styles.btn}>
-            <Text>Register</Text>
-            </Pressable>
-            </View>
-        </View>
-       </View>
-      </View>
+export default function App() {
+  return (
+    <View>
+      <Login/>
     </View>
-    </>
-  )
+  );
 }
 
-
 const styles = StyleSheet.create({
-  container:{
-    backgroundColor:'#F7941D',
-    height:'100%',
-    width:'100%'
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
-
-  welcome:{
-    display:'flex',
-    justifyContent:'center',
-    alignItems:'center',
-    alignContent:'space-around',
-    flexDirection:'column',
-    height:'25%'
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
-
-  appName:{
-    display:"flex",
-    flexDirection:'row',
-    justifyContent:'center',
-    fontWeight:400
-  },
-
-  appNameText1:{
-    fontSize:'55'
-  },
-
-  appNameText2:{
-    fontSize:'55',
-    color:'#F7941D'
-  },
-
-  form:{
-    marginTop:120,
-    backgroundColor:"#fff",
-    width:"100%",
-    borderTopLeftRadius:50,
-    borderTopEndRadius:50,
-    height:"100%"
-  },
-  formContent:{
-    marginTop:50,
-    paddingHorizontal:20
-  },
-
-  btn:{
-    backgroundColor:"#F7941D",
-    paddingHorizontal:"37%",
-    paddingVertical:20,
-    marginVertical:20,
-    borderRadius:10
-  },
-
-  btnText:{
-    color:"white",
-    fontSize:20,
-    fontWeight:"700"
-  },
-
-  formInput:{
-    borderWidth:2,
-    padding:15,
-    borderRadius:10,
-    marginBottom:20
-  },
-
-  actions:{
-    display:'flex',
-    flexDirection:'column',
-    alignItems:'center'
-  },
-
-  actionsRegister:{
-    display:'flex',
-    flexDirection:'row',
-    alignItems:'center'
-  }
-})
-
-export default Login;
+});
