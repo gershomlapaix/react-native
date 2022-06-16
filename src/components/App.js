e96c2568ae27c5979f65939fc8ac621368c39847
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-06-16 22:38:16 +0200

    fix: app.js change

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,13 @@ import Constants from 'expo-constants';
 import * as SecureStore from 'expo-secure-store';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import RegisterScreen from './src/components/Auth/Register';
-import LoginScreen from './src/components/Auth/Login';
-import RestaurantsList from './src/components/Pages/RestaurantsList';
-import HomeScreen from './src/components/Pages/Home';
-import BottomNavScreen from './src/components/Pages/BottomNav';
-import AssetExample from './src/components/AssetExample';
+import RegisterScreen from './components/Auth/Register';
+import LoginScreen from './components/Auth/Login';
+import RestaurantsList from './components/Pages/RestaurantsList';
+import HomeScreen from './components/Pages/Home';
+import BottomNavScreen from './components/BottomNav';
+import { AuthContextProvider } from './components/context/AuthContext';
+import AppRoutes from './AppRoutes';
 
 const AuthContext = React.createContext();
 
@@ -22,78 +23,10 @@ function SplashScreen() {
   );
 }
 
-const Stack = createNativeStackNavigator();
-
-const reducer = (prevState, action) => {
-  switch (action.type) {
-    case 'RESTORE_TOKEN':
-      return {
-        ...prevState,
-        userToken: action.token,
-        isLoading: false,
-      };
-
-    case 'SIGN_IN':
-      return {
-        ...prevState,
-        isSignout: false,
-        userToken: action.token,
-      };
-
-    case 'SIGN_OUT':
-      return {
-        ...prevState,
-        isSignout: true,
-        userToken: action.token,
-      };
-  }
-};
-
-export default function App() {
-  const [state, dispatch] = React.useReducer(
-    reducer,
-    { isLoading: true, isSignout: true, userToken: null } // initial state
-  );
-
-  React.useEffect(() => {
-    const bootstrapAsync = async () => {
-      let userToken;
-
-      try {
-        userToken = await SecureStore.getItemAsync('user-token');
-      } catch (err) {
-        console.log(err);
-      }
-
-      dispatch({ type: 'RESTORE_TOKEN', token: userToken });
-    };
-
-    bootstrapAsync();
-  }, []);
-
-  const authContext = React.useMemo(() => ({
-    signin: async (data) => {
-      dispatch({ type: 'SIGN_IN', token: 'dummy-token' });
-    },
-    signOut: () => dispatch({ type: 'SIGN_OUT' }),
-    signUp: async (data) => {
-      dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
-    },
-  }));
-
+export default function () {
   return (
-    <AuthContext.Provider value={authContext}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{ title: 'Supa menu app' }}
-          />
-          <Stack.Screen name="Register" component={RegisterScreen} />
-          <Stack.Screen name="Bnav" component={BottomNavScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </AuthContext.Provider>
+    <AuthContextProvider>
+      <AppRoutes />
+    </AuthContextProvider>
   );
 }
